4f54bdc02fdb8e71689e1656cb272bae9bf0ea1f
YeuVoBan14 <nhathung142002@gmail.com>
2024-09-05 08:26:25 +0700

    5/9/2024

--- src/app/configure/upload/page.tsx
+++ src/app/configure/upload/page.tsx
@@ -1,23 +1,46 @@
 "use client"
 
 import { Progress } from "@/components/ui/progress"
+import { useToast } from "@/hooks/use-toast"
+import { useUploadThing } from "@/lib/uploadthing"
 import { cn } from "@/lib/utils"
 import { Image, Loader2, MousePointerSquareDashed } from "lucide-react"
+import { useRouter } from "next/navigation"
 import { useState, useTransition } from "react"
 import Dropzone, {FileRejection} from "react-dropzone"
 
 const Page = () => {
-
+    const { toast } = useToast() 
     const [isDragOver, setIsDragOver] = useState<boolean>(false)
     const [uploadProgress, setUploadProgress] = useState<number>(0)
+    const router = useRouter()
 
-    const onDropRejected = () => {
-        console.log("failed")
+    const {startUpload, isUploading} = useUploadThing("imageUploader", {
+        onClientUploadComplete: ([data]) => {
+            const configId = data.serverData.configId
+            startTransition(() => {
+                router.push(`/configure/design?id=${configId}`)
+            })
+        },
+        onUploadProgress(p) {
+            setUploadProgress(p)
+        },
+    })
+
+    const onDropRejected = (rejectedFiles: FileRejection[]) => {
+        const [file] = rejectedFiles
+        setIsDragOver(false)
+        toast({
+            title: `${file.file.type} type is not supported.`,
+            description: "Please choose a PNG, JPG, JPEG image instead.",
+            variant: "destructive"
+        })
     }
-    const onDropAccepted = () => {
-        console.log("accepted")
+    const onDropAccepted = (acceptedFiles: File[]) => {
+        startUpload(acceptedFiles, {configId: undefined})
+        setIsDragOver(false)
     }
-    const isUploading = false
+
     const [isPending, startTransition] = useTransition()
 
     return <div className={cn("relative h-full flex-1 my-16 w-full rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/5 lg-rounded-2xl flex justify-center flex-col items-center", {
